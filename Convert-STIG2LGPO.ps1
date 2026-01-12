18b08a0ea2072afe6219fba8124501d7623627a2
Grant Trowbridge <trowbridgegolf@gmail.com>
2026-01-11 21:53:17 -0500

    Added archive expansion and XmlDocument object

--- Convert-STIG2LGPO.ps1
+++ Convert-STIG2LGPO.ps1
@@ -1,14 +1,50 @@
-function Convert-STIG2LGPO {
-    [CmdletBinding()]
-    param(
-        [Parameter(
-            Mandatory=$true,
-            Position=0,
-            HelpMessage="Path to the STIG archive"
-        )]
-        [ValidateNotNullOrEmpty()]
-        [string]$STIGPath
-    )
-    Write-Verbose "Expanding `"$STIGPath`"..."
-    Expand-Archive -Path $STIGPath -DestinationPath ($STIGPath | Split-Path -Parent)
+[CmdletBinding()]
+param(
+    [Parameter(
+        Mandatory=$true,
+        Position=0,
+        HelpMessage="Path to the STIG archive"
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string]$STIGPath,
+
+    [parameter(
+        Mandatory=$false,
+        Position=1,
+        HelpMessage="Path to the destination LGPO text file"
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string]$LGPOPath
+)
+
+if(-not $LGPOPath)
+{
+    $LGPOPath = "$PSScriptRoot\STIG2LGPOFile_$(Get-Date -Format 'yyyy-MM-dd_HH-mm-ss-fff')"
 }
+# For time stamping Get-Date -Format 'yyyy-MM-dd_HH-mm-ss-fff' (Use colons for verbose output?)
+$EXPANDPATH = ($STIGPath | Split-Path -Parent) + '\' + (Get-ChildItem -Path $STIGPath).BaseName
+Write-Verbose "Expanding `"$STIGPath`"..."
+try
+{
+    Expand-Archive -Path $STIGPath -DestinationPath $EXPANDPATH -ErrorAction Stop
+}
+catch
+{
+    Write-Error $PSItem
+}
+
+try
+{
+    $XmlFile = Get-ChildItem -Path "$EXPANDPATH\*" -Include "*.xml" -Recurse
+}
+catch
+{
+    Write-Error $PSItem
+}
+
+Write-Verbose "$($XmlFile.FullName)"
+
+$XmlData = New-Object -TypeName System.Xml.XmlDocument
+$XmlData.Load($XmlFile.FullName)
+
+# Need to determine namespace of the Group id elements
